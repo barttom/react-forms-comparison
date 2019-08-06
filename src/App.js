facc994e6a6cf1aaf3a60963e5dc6a4fc2b05444
Bart Tomczyk <bartlomiejtomczyk@gmail.com>
2019-08-06 20:52:00 +0200

    Initto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bulma/css/bulma.css'
+
+function App() {
+  return (
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">
+            React form comparing
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
